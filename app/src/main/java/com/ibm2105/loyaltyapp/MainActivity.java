4113c342b82ff7198c22cde2de2042734d1a68fb
AlexLander123 <13492360+AlexLander123@users.noreply.github.com>
2020-10-02 03:05:52 +0800

    Added Navigation Drawer and Toolbar

--- app/src/main/java/com/ibm2105/loyaltyapp/MainActivity.java
+++ app/src/main/java/com/ibm2105/loyaltyapp/MainActivity.java
@@ -1,14 +1,40 @@
 package com.ibm2105.loyaltyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
+    private DrawerLayout drawerLayout;
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        drawerLayout = findViewById(R.id.drawer_layout);
+        appBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_news, R.id.nav_pre_order, R.id.nav_branch, R.id.nav_points, R.id.nav_profile).setOpenableLayout(drawerLayout).build();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+
     }
+
 }
