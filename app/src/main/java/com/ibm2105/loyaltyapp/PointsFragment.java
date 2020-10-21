826e74af97bf228e0b1ea47db3af0f328c748865
zenzen26 <thamzien@gmail.com>
2020-10-21 13:21:08 +0800

    changes manifest, app, points, branch

--- app/src/main/java/com/ibm2105/loyaltyapp/PointsFragment.java
+++ app/src/main/java/com/ibm2105/loyaltyapp/PointsFragment.java
@@ -1,64 +1,111 @@
 package com.ibm2105.loyaltyapp;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PointsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PointsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public PointsFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PointsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PointsFragment newInstance(String param1, String param2) {
+    public static PointsFragment newInstance() {
         PointsFragment fragment = new PointsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       return inflater.inflate(R.layout.fragment_points,container,false);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_points, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //REDEEM CODE DIALOG BUTTON TOGGLE
+        final Dialog redeemDialog;
+        final Button redeemButtonDialog;
+        final ImageView redeemButtonDialogClose;
+
+        redeemButtonDialog=(Button)view.findViewById(R.id.buttonRedeemCode);
+        redeemDialog= new Dialog(view.getContext());
+
+        redeemButtonDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowRedeemDialog();
+            }
+            public void ShowRedeemDialog(){
+              redeemDialog.setContentView(R.layout.points_dialogbox);
+              ImageView  redeemButtonDialogClose=(ImageView)redeemDialog.findViewById(R.id.closeIconButton);
+              Button  redeemButtonSubmit=(Button)redeemButtonDialog.findViewById(R.id.buttonRedeemSubmit);
+              TextView  redeemContent=(TextView)redeemButtonDialog.findViewById(R.id.redeemPointsContent);
+              EditText redeemInput=(EditText)redeemButtonDialog.findViewById(R.id.redeemCodeInput);
+
+                redeemButtonDialogClose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        redeemDialog.dismiss();
+                    }
+                });
+
+                redeemDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                redeemDialog.show();
+            }
+        });
+
+
+
+        //POINTS ITEM CODE
+        final PointsListItem[]pointsListItems=new PointsListItem[]{
+                new PointsListItem("Free Brownie!",R.drawable.ic_baseline_stars_24,100),
+                new PointsListItem("Free Brownie!",R.drawable.ic_baseline_account_circle_24,100),
+                new PointsListItem("Free Brownie!",R.drawable.ic_baseline_stars_24,100),
+                new PointsListItem("Free Brownie!",R.drawable.ic_baseline_account_circle_24,100),
+                new PointsListItem("Free Brownie!",R.drawable.ic_baseline_stars_24,100),
+                new PointsListItem("Free Brownie!",R.drawable.ic_baseline_account_circle_24,100),
+                new PointsListItem("Free Brownie!",R.drawable.ic_baseline_stars_24,100),
+                new PointsListItem("Free Brownie!",R.drawable.ic_baseline_account_circle_24,100),
+                new PointsListItem("Free Brownie!",R.drawable.ic_baseline_stars_24,100),
+                new PointsListItem("Free Brownie!",R.drawable.ic_baseline_account_circle_24,100),
+                new PointsListItem("Free Brownie!",R.drawable.ic_baseline_stars_24,100),
+                new PointsListItem("Free Brownie!",R.drawable.ic_baseline_account_circle_24,100),
+                new PointsListItem("Free Brownie!",R.drawable.ic_baseline_stars_24,100),
+                new PointsListItem("Free Brownie!",R.drawable.ic_baseline_account_circle_24,100),
+
+        };
+
+        final RecyclerView recyclerView=(RecyclerView)view.findViewById(R.id.points_recyclerview);
+        PointsListAdapter pointsListAdapter=new PointsListAdapter(pointsListItems);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(pointsListAdapter);
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 }
